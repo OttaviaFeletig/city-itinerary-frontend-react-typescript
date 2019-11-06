1e2864a6d392b59b20c2b8829f69135b887a3cd7
OttaviaFeletig <ottafel@hotmail.it>
2019-11-06 19:14:39 +0100

    navigation implemented

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import { Home, LocationCity } from "@material-ui/icons";
 import { navigationStyle } from "../styles/Styles";
+import { Link } from "react-router-dom";
+
 const Navigation: React.FC = () => {
   const { banner, icons } = navigationStyle();
+  const [value, setValue] = React.useState("recents");
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
+  };
   return (
-    <BottomNavigation className={banner}>
+    <BottomNavigation className={banner} value={value} onChange={handleChange}>
       <BottomNavigationAction
+        component={Link}
+        to="/"
         className={icons}
         label="Home"
         value="home"
         icon={<Home />}
       />
       <BottomNavigationAction
+        component={Link}
+        to="/cities"
         className={icons}
         label="Cities"
         value="cities"
